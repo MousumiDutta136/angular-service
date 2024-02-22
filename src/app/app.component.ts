f5691c9951b15b60202184f29081e59b00e87f33
Mousumi <dutta.mousumi136@gmail.com>
2024-02-22 13:13:07 -0500

    angular services for active/non active users

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { UsersService } from './services/users.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers:[UsersService]
+})
+export class AppComponent {
+  inActiveUsers: [] = [];
+  activeUsers: [] = [];
+  constructor(usersService:UsersService){
+
+  }
+
+}
